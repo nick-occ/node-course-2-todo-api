93c45dd3d7ab3c13a25ce5eea9cab0a99fe3a90d
nick-occ <nicholaso1980@gmail.com>
2018-07-09 22:03:40 -0400

    Add GET /users/me and authentication

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -96,6 +97,7 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// POST /users
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ["email", "password"]);
     var user = new User(body);
@@ -108,6 +110,12 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+   res.send(req.user);
+})
 
 
 app.listen(port, () => {
